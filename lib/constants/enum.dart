b4849217b1456fcb801759cdba375d3823cc2dc8
Vatsal Bhesaniya <bhesaniyavatsal@gmail.com>
2023-02-16 17:35:28 +0530

    Added input field widget.

--- lib/constants/enum.dart
+++ lib/constants/enum.dart
@@ -1,1 +1,11 @@
 enum HttpMethod { post, get, put, delete, patch }
+
+enum InputFieldHeight {
+  small,
+  large,
+}
+
+enum InputFieldBorderType {
+  outlineInputBorder,
+  underlineInputBorder,
+}
