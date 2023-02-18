0f631e171338a5c1cc2a840f7cc4573b65115493
Vatsal Bhesaniya <bhesaniyavatsal@gmail.com>
2023-02-18 21:05:53 +0530

    Added enum for member role.

--- lib/extentions/extensions.dart
+++ lib/extentions/extensions.dart
@@ -1,12 +1,27 @@
 import 'package:pma/constants/enum.dart';
 
-extension TextFieldHeightExtendion on InputFieldHeight {
+extension TextFieldHeightExtension on InputFieldHeight {
   double get verticalPadding {
     switch (this) {
       case InputFieldHeight.small:
         return 12;
       case InputFieldHeight.large:
         return 16;
+    }
+  }
+}
+
+extension MemberRoleExtension on MemberRole {
+  String get title {
+    switch (this) {
+      case MemberRole.owner:
+        return 'Owner';
+      case MemberRole.admin:
+        return 'Admin';
+      case MemberRole.member:
+        return 'Member';
+      case MemberRole.guest:
+        return 'Guest';
     }
   }
 }
