54aaa156bb9ffa6daae60c290b7ce61e7fd5c44f
Vatsal Bhesaniya <bhesaniyavatsal@gmail.com>
2023-03-07 10:17:55 +0530

    Updated theme colors.

--- lib/theme/pma_theme.dart
+++ lib/theme/pma_theme.dart
@@ -5,17 +5,17 @@ ThemeData buildLightTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: _lightColorScheme,
-    primaryColor: pink100,
-    scaffoldBackgroundColor: backgroundWhite,
-    cardColor: backgroundWhite,
-    textSelectionTheme: const TextSelectionThemeData(selectionColor: pink100),
+    primaryColor: oldRose,
+    scaffoldBackgroundColor: seashell,
+    cardColor: paleDogwood,
+    textSelectionTheme: const TextSelectionThemeData(selectionColor: mistyRose),
     buttonTheme: const ButtonThemeData(
       colorScheme: _lightColorScheme,
     ),
-    primaryIconTheme: _buildIconTheme(base.iconTheme),
     textTheme: _buildTextTheme(base.textTheme),
     primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
-    iconTheme: _buildIconTheme(base.iconTheme),
+    iconTheme: base.iconTheme.copyWith(color: oldRose),
+    primaryIconTheme: base.iconTheme.copyWith(color: oldRose),
   );
 }
 
@@ -23,50 +23,50 @@ ThemeData buildDarkTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: _darkColorScheme,
-    primaryColor: pink100,
-    scaffoldBackgroundColor: backgroundWhite,
-    cardColor: backgroundWhite,
-    textSelectionTheme: const TextSelectionThemeData(selectionColor: pink100),
+    primaryColor: raisinBlack,
+    scaffoldBackgroundColor: timberwolf,
+    cardColor: peachYellow,
+    textSelectionTheme: const TextSelectionThemeData(selectionColor: dimGray),
     buttonTheme: const ButtonThemeData(
       colorScheme: _darkColorScheme,
     ),
-    primaryIconTheme: _buildIconTheme(base.iconTheme),
     textTheme: _buildTextTheme(base.textTheme),
     primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
-    iconTheme: _buildIconTheme(base.iconTheme),
+    iconTheme: base.iconTheme.copyWith(color: raisinBlack),
+    primaryIconTheme: base.iconTheme.copyWith(color: raisinBlack),
   );
 }
 
 const ColorScheme _lightColorScheme = ColorScheme(
-  primary: Colors.amber,
-  primaryContainer: brown900,
-  secondary: Colors.lightBlue,
-  secondaryContainer: brown900,
-  surface: surfaceWhite,
-  background: backgroundWhite,
+  primary: oldRose,
+  onPrimary: surfaceWhite,
+  primaryContainer: mistyRose,
+  secondary: paleDogwood,
+  onSecondary: surfaceWhite,
+  secondaryContainer: paleDogwood,
+  surface: platinum,
+  onSurface: platinum,
+  background: seashell,
+  onBackground: seashell,
   error: errorRed,
-  onPrimary: brown900,
-  onSecondary: brown900,
-  onSurface: Colors.blueGrey,
-  onBackground: Colors.grey,
-  onError: surfaceWhite,
+  onError: errorRed,
   outline: Colors.blueGrey,
   brightness: Brightness.light,
 );
 
 const ColorScheme _darkColorScheme = ColorScheme(
-  primary: Colors.yellow,
-  primaryContainer: Colors.red,
-  secondary: Colors.redAccent,
-  secondaryContainer: Colors.lime,
-  surface: Colors.white24,
-  background: Colors.white30,
-  error: Colors.deepOrange,
-  onPrimary: Colors.pinkAccent,
-  onSecondary: Colors.lime,
-  onSurface: Colors.blueGrey,
-  onBackground: Colors.grey,
-  onError: Colors.red,
+  primary: raisinBlack,
+  onPrimary: raisinBlack,
+  primaryContainer: dimGray,
+  secondary: peachYellow,
+  onSecondary: peachYellow,
+  secondaryContainer: peachYellow,
+  surface: raisinBlack,
+  onSurface: surfaceWhite,
+  background: timberwolf,
+  onBackground: timberwolf,
+  error: errorRed,
+  onError: errorRed,
   outline: Colors.blueGrey,
   brightness: Brightness.dark,
 );
@@ -80,21 +80,15 @@ TextTheme _buildTextTheme(TextTheme baseTheme) {
   );
 }
 
-IconThemeData _buildIconTheme(IconThemeData original) {
-  return original.copyWith(color: brown900);
-}
-
-const Color pink50 = Color(0xFFFEEAE6);
-const Color pink100 = Color(0xFFFEDBD0);
-const Color pink300 = Color(0xFFFBB8AC);
-const Color pink400 = Color(0xFFEAA4A4);
-
-const Color brown900 = Color(0xFF442B2D);
-const Color brown600 = Color(0xFF7D4F52);
-
-const Color errorRed = Color(0xFFC5032B);
-
 const Color surfaceWhite = Color(0xFFFFFBFA);
-const Color backgroundWhite = Colors.white;
-
-const double defaultLetterSpacing = 0.03;
+const Color errorRed = Color(0xFFC5032B);
+const Color timberwolf = Color(0xFFDAD5D4);
+const Color paleDogwood = Color(0xFFF3C5B7);
+const Color peachYellow = Color(0xFFFBDEAC);
+const Color platinum = Color(0xFFE5E3E3);
+const Color dimGray = Color(0xFF776B71);
+const Color black = Color(0xFF000000);
+const Color mistyRose = Color(0xFFF5D7D5);
+const Color raisinBlack = Color(0xFF2F303E);
+const Color oldRose = Color(0xFFAD7775);
+const Color seashell = Color(0xFFFCEEEA);
