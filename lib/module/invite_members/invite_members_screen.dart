9afda2a36dbeafee70d2f3b34fb68c2df25d5be9
Vatsal Bhesaniya <bhesaniyavatsal@gmail.com>
2023-03-15 17:57:01 +0530

    Refactor invite members repository.

--- lib/module/invite_members/invite_members_screen.dart
+++ lib/module/invite_members/invite_members_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router_flow/go_router_flow.dart';
+import 'package:pma/config/dio_config.dart';
 import 'package:pma/models/search_user.dart';
 import 'package:pma/module/invite_members/bloc/invite_members_bloc.dart';
 import 'package:pma/module/invite_members/invite_members_repository.dart';
@@ -33,7 +34,8 @@ class _InviteMembersScreenState extends State<InviteMembersScreen> {
         child: BlocProvider<InviteMembersBloc>(
           create: (BuildContext context) => InviteMembersBloc(
             inviteMembersRepository: InviteMembersRepository(
-              dioClient: context.read<DioClient>(),
+              dio: context.read<Dio>(),
+              dioConfig: context.read<DioConfig>(),
             ),
           ),
           child: BlocConsumer<InviteMembersBloc, InviteMembersState>(
